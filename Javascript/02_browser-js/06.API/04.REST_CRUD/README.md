c5cab537258fba33da03fc28cc14f0cbea1eabe2
CodeWithSub <subhransusahoo020@gmail.com>
2025-08-31 09:38:53 +0530

    Add repo 04.REST_CRUD(A basic CRUD operation)

---
+++ Javascript/02_browser-js/06.API/04.REST_CRUD/README.md
@@ -0,0 +1,31 @@
+# Simple Express.js Posts CRUD App
+
+A basic CRUD (Create, Read, Update, Delete) application built with **Express.js** and **EJS** for rendering views.  
+This project demonstrates how to build a simple blogging/posting system with in-memory storage.
+
+---
+
+## Features
+- View all posts
+- Create a new post
+- View details of a single post
+- Edit an existing post
+- Delete a post
+
+---
+
+## Tech Stack
+- [Node.js](https://nodejs.org/)
+- [Express.js](https://expressjs.com/)
+- [EJS](https://ejs.co/)
+- [UUID](https://www.npmjs.com/package/uuid) (for unique post IDs)
+- [Method-Override](https://www.npmjs.com/package/method-override) (for PATCH/DELETE requests in forms)
+
+---
+
+## Installation & Setup
+
+1. Clone the repository:
+   ```bash
+   git clone <your-repo-link>
+   cd <your-project-folder>
---
+++ Javascript/02_browser-js/06.API/04.REST_CRUD/index.js
@@ -0,0 +1,86 @@
+const express = require('express');
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+const path = require("path")
+app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+const { v4: uuidv4 } = require('uuid');
+const methodOverride = require("method-override")
+
+app.use(express.static(path.join(__dirname, "public")));
+app.use(methodOverride('_method'))
+
+
+let posts = [
+  {
+    id: uuidv4(),
+    username: "subhransu",
+    content: "Hello there"
+  },
+  {
+    id: uuidv4(),
+    username: "ritesh",
+    content: "Happy coding"
+  },
+  {
+    id: uuidv4(),
+    username: "bunty",
+    content: "oye oye"
+  }
+]
+
+
+app.get("/posts", (req, res) => {
+  res.render("index.ejs", { posts })
+})
+
+// new route
+
+app.get("/posts/new", (req, res) => {
+
+  res.render("new.ejs");
+})
+
+app.post("/posts", (req, res) => {
+  let id = uuidv4();
+  let { username, content } = req.body;
+  posts.push({ username, content, id });
+  res.redirect("/posts")
+})
+
+// show route
+
+app.get("/posts/:id", (req, res) => {
+  let { id } = req.params;
+  let post = posts.find((p) => id === p.id);
+  res.render("show.ejs", { post });
+})
+
+//update route
+
+app.patch("/posts/:id", (req, res) => {
+  let { id } = req.params;
+  let newContent = req.body.content
+  let post = posts.find((p) => id === p.id);
+  post.content = newContent;
+  res.redirect("/posts")
+})
+
+// Edit route
+app.get("/posts/:id/edit", (req, res) => {
+  let { id } = req.params;
+  let post = posts.find((p) => id === p.id);
+  res.render("edit.ejs", { post })
+})
+
+// Delete route
+app.delete("/posts/:id", (req, res) => {
+  let { id } = req.params;
+  posts = posts.filter((p) => p.id !== id); // remove post with matching id
+  res.redirect("/posts");
+});
+
+let port = 8080;
+app.listen(port, (req, res) => {
+  console.log("App is listning at port " + port);
+})
