8f183759742f93906d1dab5cbdc25ba9b0bd5c1b
CodeWithSub <subhransusahoo020@gmail.com>
2025-10-02 20:12:38 +0530

    modified 02customer.js in MongoDB

--- MongoDB/Relationships/02customer.js
+++ MongoDB/Relationships/02customer.js
@@ -16,6 +16,7 @@ const orderSchema = new mongoose.Schema({
   price: Number
 });
 
+
 const customerSchema = new mongoose.Schema({
   name: String,
   orders: [
@@ -26,18 +27,27 @@ const customerSchema = new mongoose.Schema({
   ]
 });
 
+customerSchema.post("findOneAndDelete", async (customer) => {
+  if (customer.orders.length) {
+    let result = await Order.deleteMany({ _id: { $in: customer.orders } });
+    console.log(result);
+  }
+});
+
 const Order = mongoose.model("Order", orderSchema);
 const Customer = mongoose.model("Customer", customerSchema)
 
 const addCustomer = async () => {
   let cust1 = new Customer({
-    name: "Subhransu"
+    name: "Bunty"
   });
-  let order1 = await Order.findOne({ item: "Samosa" });
-  let order2 = await Order.findOne({ item: "Cofee" });
+  let order1 = await Order.findOne({ item: "Cold cofee" });
+  let order2 = await Order.findOne({ item: "Jalebi" });
+  let order3 = await Order.findOne({ item: "Chai" });
 
   cust1.orders.push(order1)
   cust1.orders.push(order2)
+  cust1.orders.push(order3)
 
   let result = await cust1.save();
   console.log(result);
@@ -45,24 +55,34 @@ const addCustomer = async () => {
 }
 
 const findCustomer = async () => {
-
   let result = await Customer.find({}).populate("orders");
   console.log(result[0]);
 }
 
-// const addOrders = async () => {
-//   try {
-//     let res = await Order.insertMany(
-//     [{ item: "Samosa", price: 12 },
-//     { item: "Pizza", price: 100 },
-//     { item: "Cofee", price: 60 }]
-//   );
-//   console.log(res);
-//   } catch (err) {
-//     console.log(err);
-//   }
-// };
+const addOrders = async () => {
+  try {
+    let res = await Order.insertMany(
+      [{ item: "Jalebi", price: 6 },
+      { item: "Cold cofee", price: 90 },
+      { item: "Cofee", price: 60 },
+      { item: "Burger", price: 80 },
+      { item: "Chowmin", price: 40 }]
+    );
+    console.log(res);
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 // addOrders();
 // addCustomer();
-findCustomer()
+// findCustomer();
+
+// Handling Deletion
+
+const delCust = async () => {
+  let data = await Customer.findByIdAndDelete("68d7b33fac0f99f7235c5ff6");
+  console.log(data);
+}
+
+delCust();
